3e85b3e3473e31e03ace9bf7cef296bd9176d4ac
Kevin Winata <kevinwinatamichael@gmail.com>
2019-02-19 13:56:53 +0800

    Add search bar

--- my_collection/urls.py
+++ my_collection/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='collection_index'),
     path('<int:item_id>/', views.detail, name='item_detail'),
+    path('search', views.search, name='search')
 ]
--- my_collection/views.py
+++ my_collection/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Item
+from django.http import HttpResponse
+from django.db.models import Q
 
 def index(request):
     latest_item = Item.objects.all()
@@ -14,3 +16,35 @@ def detail(request, item_id):
     return render(request, 'item_detail.html', {
         'item': item,
     })
+
+
+def search(request):
+    if request.method == 'GET':
+        items = ""
+        text=""
+        query = request.GET.get('query')
+        if query:
+            items = Item.objects.filter(
+                Q(name__contains=query)|
+                Q(location__contains=query)|
+                Q(material_type__contains=query)|
+                Q(description__contains=query)
+            )
+            if items.count()==0:
+                text = "Cannot find result for '" + query + "'"
+            else:
+                text = "Search result for: '" + query + "'"
+            return render(request, 'index.html', {
+                'items': items,
+                'search_text': text,
+            })
+        else:
+            items = Item.objects.all()
+            text = "All items"
+            return render(request, 'index.html', {
+                'items': items,
+                'search_text': text,
+            })
+    else:
+        return HttpResponse(404)
+
